6d84411d40a1ad4ac9b82ca811241996e32806c8
Samanvaya Bhardwaj <samanvayabhardwaj2001@gmail.com>
2023-05-15 21:27:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BarChart } from "recharts";
+import Main from "./component/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+      <BarChart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/AnotherComponent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
+
+function AnotherComponent({ wordData }) {
+  console.log(wordData);
+
+  return (
+    <div>
+      <BarChart width={1000} height={500} data={wordData}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey={'word'} margin={{ bottom: 10 }} />
+        <YAxis dataKey={'frequency'} margin={{ left: 10 }} />
+        <Tooltip />
+        <Legend wrapperStyle={{ paddingTop: 20 }} />
+        <Bar dataKey={'frequency'} fill='orange' />
+      </BarChart>
+    </div>
+  );
+}
+
+export default AnotherComponent;
